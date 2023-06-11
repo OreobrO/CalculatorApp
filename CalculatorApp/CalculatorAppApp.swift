9fc4a4fa108c1d186f702f3dfdcf39bc1fe63d1e
henry.choi <oreobro@naver.com>
2023-06-11 15:21:26 +0900

    Initial Commit

---
+++ CalculatorApp/CalculatorAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalculatorAppApp.swift
+//  CalculatorApp
+//
+//  Created by 최민규 on 2023/06/11.
+//
+
+import SwiftUI
+
+@main
+struct CalculatorAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
