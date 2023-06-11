b2335baf128fa0a62c2cb080a06ee72a6cdfa541
henry.choi <oreobro@naver.com>
2023-06-11 18:52:55 +0900

    [FEAT] Calculator 기본연산 기능

--- CalculatorApp/ContentView.swift
+++ CalculatorApp/ContentView.swift
@@ -18,12 +18,13 @@ enum CalcButton: String {
     case eight = "8"
     case nine = "9"
     case zero = "0"
+    case doubleZero = "00"
     case add = "+"
     case subtract = "-"
     case divide = "/"
     case multiply = "x"
     case equal = "="
-    case clear = "AC"
+    case clear = "C"
     case decimal = "."
     case percent = "%"
     case negative = "-/+"
@@ -33,33 +34,40 @@ enum CalcButton: String {
         case .add, .subtract, .multiply, .divide, .equal:
             return .main
         case .clear, .negative, .percent:
-            return Color.gray3
+            return .gray3
         default:
-            return Color.gray2
+            return .gray2
         }
     }
     
     var fontColor: Color {
         switch self {
         case .add, .subtract, .multiply, .divide, .equal:
-            return .main
+            return .white
         case .clear, .negative, .percent:
-            return Color.gray3
+            return .black
         default:
-            return Color.gray2
+            return .black
         }
     }
 }
 
+enum Operation {
+    case add, subtract, multiply, divide, none
+}
 
 struct ContentView: View {
+    
+    @State var value = "0"
+    @State var runningNumber = 0
+    @State var currentOperation: Operation = .none
     
     let buttons: [[CalcButton]] = [
         [.clear, .negative, .percent, .divide],
         [.seven, .eight, .nine, .multiply],
         [.four, .five, .six, .subtract],
         [.one, .two, .three, .add],
-        [.zero, .decimal, .equal],
+        [.zero, .doubleZero, .decimal, .equal],
     ]
     var body: some View {
         ZStack {
@@ -68,41 +76,96 @@ struct ContentView: View {
             VStack {
                 Spacer()
                 //Text display
-                HStack {
-                    Spacer()
-                    Text("0")
-                        .bold()
-                        .font(.system(size: 64))
-                        .foregroundColor(.white)
-                }//Result
-                .padding()
-                
+                ZStack {
+                    RoundedRectangle(cornerRadius: 20)
+                        .frame(height: 100)
+                        .foregroundColor(.gray2)
+                    
+                    HStack {
+                        Text("w")
+                            .font(.system(size: 32, weight: .light))
+                            .foregroundColor(.black)
+                            .opacity(0.5)
+                        Spacer()
+                        Text(value)
+                            .font(.system(size: 48, weight: .light))
+                            .foregroundColor(.black)
+                    }//Result
+                    .padding(.horizontal, 24)
+                }
                 // Our buttons
                 ForEach(buttons, id: \.self) { row in
                     HStack(spacing: 12) {
                         ForEach(row, id: \.self) { item in
                             Button(action: {
-                                
+                                self.didTap(button: item)
                             }, label: {
                                 Text(item.rawValue)
                                     .font(.system(size: 32))
                                     .frame(
                                         width: self.buttonWidth(item: item),
-                                        height: self.buttonHeight() )
+                                        height: max(48, self.buttonHeight())
+                                    )
                                     .background(item.buttonColor)
-                                    .foregroundColor(.white)
-                                    .cornerRadius(self.buttonHeight()/2)
+                                    .foregroundColor(item.fontColor)
+                                    .cornerRadius(max(48, self.buttonHeight())/2)
                             })
                         }//ForEach item
                     }//HStack
                 }//ForEach row
                 .padding(.bottom, 4)
             }//VStack
+            .padding()
         }
     }
     
-    func didTap(Button: CalcButton) {
-        
+    func didTap(button: CalcButton) {
+        switch button {
+        case .add, .subtract, .multiply, .divide, .equal:
+            if button == .add {
+                self.currentOperation = .add
+                self.runningNumber = Int(self.value) ?? 0
+            }
+            else if button == .subtract {
+                self.currentOperation = .subtract
+                self.runningNumber = Int(self.value) ?? 0
+            }
+            else if button == .multiply {
+                self.currentOperation = .multiply
+                self.runningNumber = Int(self.value) ?? 0
+            }
+            else if button == .divide {
+                self.currentOperation = .divide
+                self.runningNumber = Int(self.value) ?? 0
+            }
+            else if button == .equal {
+                let runningValue = self.runningNumber
+                let currentValue = Int(self.value) ?? 0
+                switch self.currentOperation {
+                case .add: self.value = "\(runningValue + currentValue)"
+                case .subtract: self.value = "\(runningValue - currentValue)"
+                case .multiply: self.value = "\(runningValue * currentValue)"
+                case .divide: self.value = "\(runningValue / currentValue)"
+                case .none:
+                    break
+                }
+            }
+            if button != .equal {
+                self.value = "0"
+            }
+        case .clear:
+            self.value = "0"
+        case .decimal, .negative, .percent:
+            break
+        default:
+            let number = button.rawValue
+            if self.value == "0" {
+                value = number
+            }
+            else {
+                self.value = "\(self.value)\(number)"
+            }
+        }
     }
     
     func buttonWidth(item: CalcButton) -> CGFloat {
@@ -110,7 +173,7 @@ struct ContentView: View {
     }
     
     func buttonHeight() -> CGFloat {
-        return (UIScreen.main.bounds.height * 2 / 5 - (6*12)) / 5
+        return (UIScreen.main.bounds.height * 0.37 - (6*12)) / 5
     }
     
 }
